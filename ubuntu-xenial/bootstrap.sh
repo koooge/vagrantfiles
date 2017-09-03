0e7e4a595a054054192f7b610bd95feb70e0bd1f
koooge <koooooge@gmail.com>
2017-09-04 01:47:29 +0900

    fix add git

--- ubuntu-xenial/bootstrap.sh
+++ ubuntu-xenial/bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+sudo apt-get -qq update
+sudo apt-get -y install git
+
 git clone https://github.com/koooge/ubuntu-xenial-provisioning.git
 cd ubuntu-xenial-provisioning
 ./ubuntu.sh

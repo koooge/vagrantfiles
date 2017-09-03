c0b376e9b489e04b205fbb13b2726569bd77756d
koooge <koooooge@gmail.com>
2017-09-04 00:34:12 +0900

    use envimation/ubuntu-xenial because ubuntu/xenial doesn't support vagrant user

--- ubuntu-xenial/bootstrap.sh
+++ ubuntu-xenial/bootstrap.sh
@@ -1,16 +1,7 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 
-PASSWORD=$(perl -e 'print crypt('vagrant', "\$6\$");')
-sudo useradd -p ${PASSWORD} -G sudo -m -s /bin/bash vagrant
-
-sudo apt-get -qq update
-sudo apt-get -y upgrade
-
-sudo apt-get install --force-yes -y git
-
-
-# git clone https://github.com/koooge/debian-provisioning.git
-# cd debian-provisioning
-# ./debian.sh
-# cd ..
-# rm -r debian-provisioning
+git clone https://github.com/koooge/ubuntu-xenial-provisioning.git
+cd ubuntu-xenial-provisioning
+./ubuntu.sh
+cd -
+# rm -rf ubuntu-xenial-provisioning
